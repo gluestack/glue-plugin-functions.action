453b315d8aa14f67ad46f2b0ae43362eaf572203
Faiz A. Farooqui <faizz.af@gmail.com>
2023-01-16 14:47:28 +0530

    chore(v0.0.9): added handle for glue public

--- template/server.js
+++ template/server.js
@@ -14,12 +14,38 @@ app.get('/health-check', (req, res) => {
   return res.status(200).json({status: true, message: 'Health check performed successfully'});
 });
 
-app.post('/functions', (req, res) => {
+const middleware = (req, res, next) => {
+  const { headers } = req;
+
+  if (! headers.hasOwnProperty('x-glue-invoke')) {
+    return next();
+  }
+
+  const { 'x-glue-invoke': invoke } = headers;
+  const isPublic = process.env.GLUE_PUBLIC === 'false' ? false : true;
+  if (invoke !== 'client' || isPublic === true) {
+    return next();
+  }
+
+  //
+  // If GLUE_PUBLIC is set specifically to "false". This means that the function
+  // is not publically exposed to client invoke calls.
+  //
+  // If it is set "true" or the env var does not exist, then it is assumed that
+  // the function is available for client invoke calls.
+  return res.status(401).json({
+    status: false,
+    message: 'Access denied'
+  });
+};
+
+app.post('/functions', middleware, (req, res) => {
   const { headers, body }  = req;
 
   // do something with the headers and body
   // perform your custom business logic
-  console.log({ body });
+
+  console.log({ headers, body });
 
   return res.status(200).json({
     status: true,
